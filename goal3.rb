95e5dbfed0c997a0e917bd64cee03e6bda3d1795
superrino130 <60283445+superrino130@users.noreply.github.com>
2022-06-17 06:44:13 +0900

    voteファイルのjson化

--- goal3.rb
+++ goal3.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require 'nokogiri'
 require 'open-uri'
+require 'json'
 
 class Goal3 < Sinatra::Base
   not_found do
@@ -14,7 +15,13 @@ class Goal3 < Sinatra::Base
   end
   get '/:hold' do
     @title = "TOTO Goal3"
-    @odds = Goal3OddsSearch.new(params[:hold]).odds_goal3 if params[:hold].nil?.!
+    if params[:hold].nil?.!
+      Goal3OddsSearch.new(params[:hold]).odds_goal3
+      t = File.read("dat/totogoal3_" + params[:hold] + ".json") do |f|
+        JSON.load(f)
+      end
+      @odds = JSON.parse(t)
+    end
     erb :goal3
   end
 end
@@ -29,20 +36,27 @@ class Goal3OddsSearch
     html = URI.open(url){ |f| f.read }
     doc = Nokogiri::HTML.parse(html)
     
-    votes = Array.new(6){ Array.new(4, 0.0) }
-    
+    votes = Hash.new{ |h, k| h[k] = {} }
+
+    doc.css('td.td_team').each_with_index do |td, i|
+      votes[i + 1]["team_name"] = td.text
+    end
     doc.css('td.zero_bar img').each_with_index do |td, i|
-      votes[i][0] = td.attribute('title').value.to_f
+      votes[i + 1][0] = td.attribute('title').value.to_f
     end
     doc.css('td.one_bar img').each_with_index do |td, i|
-      votes[i][1] = td.attribute('title').value.to_f
+      votes[i + 1][1] = td.attribute('title').value.to_f
     end
     doc.css('td.two_bar img').each_with_index do |td, i|
-      votes[i][2] = td.attribute('title').value.to_f
+      votes[i + 1][2] = td.attribute('title').value.to_f
     end
     doc.css('td.three_bar img').each_with_index do |td, i|
-      votes[i][3] = td.attribute('title').value.to_f
+      votes[i + 1][3] = td.attribute('title').value.to_f
     end
-    votes
+
+    savefile = JSON.parse(votes.to_json)
+    File.open("dat/totogoal3_" + @hold + ".json", "w") do |f|
+      JSON.dump(savefile, f)
+    end
   end
 end
